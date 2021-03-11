e75e5021f65851cd68aea78f98033aa62e8adf69
Paweł Kuczyński <pawel.kuczynski@l4a-soft.com>
2021-03-11 15:17:12 +0100

    feat: finding free patches

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import Space from './Space';
 import Shape from './Shape';
 
-const space = new Space(0, 0, 10000, 10000);
+const space = new Space(0, 0, 500000, 500000);
 
-const shape = new Shape(0, 0, 8000, 8000);
+// const shape = new Shape(0, 0, 10000, 2000);
+// const shape1 = new Shape(0, 8000, 10000, 2000);
+// const shape2 = new Shape(0, 2000, 2000, 6000);
+// const shape3 = new Shape(8000, 2000, 2000, 6000);
 
-space.addShape(shape);
+for(let i = 0, y = 1; i<300000; i = i + y, y++) {
+  const shape = new Shape(i, i, y, y);
+  space.addShape(shape);
+}
+console.log("Number of shapes: " + space.shapes.length);
 
-console.log(space.findEmpty());
+// space.addShape(shape);
+// space.addShape(shape1);
+// space.addShape(shape2);
+// space.addShape(shape3);
+
+console.time("findEmptyLeftBound")
+
+let result = space.findEmptyLeftBound(600, 800);
+
+console.timeEnd("findEmptyLeftBound")
+
+console.log("Found shape x=%d, y=%d, width=%d, height=%d", result.x, result.y, result.width, result.height);
